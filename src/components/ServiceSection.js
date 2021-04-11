1719690889a59feb74bb53becf6a96c422a83f2e
Jovin-Sim <secretboy-18@hotmail.com>
2021-04-11 19:43:57 +0800

    6. Styling Services Section

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -4,54 +4,93 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+import { About, Image, Description } from "../styles";
+import styled from "styled-components";
 
 const ServiceSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="Clock"></img>
               <h3>Efficient</h3>
             </div>
             <p>lorem blah blah blah</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="teamwork"></img>
               <h3>Teamwork</h3>
             </div>
             <p>lorem blah blah blah</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="diaphragm"></img>
               <h3>Diaphragm</h3>
             </div>
             <p>lorem blah blah blah</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={money} alt="money"></img>
               <h3>Affordable</h3>
             </div>
             <p>lorem blah blah blah</p>
-          </div>
-        </div>
-      </div>
+          </Card>
+        </Cards>
+      </Description>
 
-      <div className="image">
+      <Image>
         <img src={home2} alt="home2"></img>
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Icon = styled.div`
+  img {
+    width: 30%;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 13rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    img {
+      width: 30%;
+      padding-right: 1rem;
+    }
+    h3 {
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServiceSection;
