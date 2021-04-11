5d061e950c7e30e4ec7cfee52013090393c00c9b
Jovin-Sim <secretboy-18@hotmail.com>
2021-04-11 18:59:15 +0800

    5. Styled Components Introduction

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+
+
+}
+body{
+    background: #1b1b1b;
+}
+button{
+    font-weight: bold;
+    font-size: 1.1.rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+    &:hover{
+        background-color: #23d997;
+        color: white;
+    }
+   
+}
+h2{
+        font-weight: lighter;
+        font-size: 3.2rem;
+
+    }
+h3{
+        color: white;
+    }
+p{
+        padding:3rem 0rem;
+        color: #ccc;
+        font-size: 1.1rem;
+    }
+h4{
+    font-weight: bold;
+}
+span{
+    font-weight: bold;
+    color:#23d997;
+}
+a{
+    font-size: 1.1rem
+}
+img{
+    width: 100%;
+
+}
+`;
+
+export default GlobalStyle;
